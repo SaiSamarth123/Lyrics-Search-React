ab660152807d4dc3bfb10ee1031c8723535671a3
SaiSamarth123 <sai003sam@gmail.com>
2023-05-23 15:06:19 -0500

    Modified how the top tracks are displayed and added a button to route to another page

--- src/components/layout/Index.js
+++ src/components/layout/Index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Tracks from '../tracks/Tracks';
+import React from "react";
+import Tracks from "../tracks/Tracks";
 const Index = () => {
   return (
-        <React.Fragment>
-            <Tracks/>
-        </React.Fragment>
-  )
-}
+    <React.Fragment>
+      <Tracks />
+    </React.Fragment>
+  );
+};
 
 export default Index;
