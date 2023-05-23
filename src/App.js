2f0f0827b77fd2483691e2e90a03193fe69bad16
SaiSamarth123 <sai003sam@gmail.com>
2023-05-23 14:40:50 -0500

    Added Top track lists and spinner for loading

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import './App.css';
 import Navbar from './components/layout/Navbar'
 import Index from './components/layout/Index'
 import React from 'react';
+import Provider from './context'
 
 function App() {
   return (
-    <Router>
-    <React.Fragment>
-      <Navbar/>
-      <div className="container">
-        <Switch>
-          <Route exact path="/" component ={Index}/>
-        </Switch>
-      </div>
-    </React.Fragment>
-   </Router>
+    <Provider>
+      <Router>
+        <React.Fragment>
+          <Navbar/>
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
+    </Provider>
   );
 }
 
---
+++ src/components/tracks/Track.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Track = (props) => {
+  return (
+    <div>
+
+
+    </div>
+  )
+}
+
+export default Track
